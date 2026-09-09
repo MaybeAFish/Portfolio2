35cf1ff76a559baaf15326a6e0ed3d7a6af3ac41
Amine El Hammdaoui <166661394+AmineElHammdaoui@users.noreply.github.com>
2026-09-09 19:23:41 +0200

    game wip

--- game/interactables/interactable-groups.js
+++ game/interactables/interactable-groups.js
@@ -11,63 +11,63 @@ export const GameInteractables = {
     {
       type: OverlayInteractable,
       position: new THREE.Vector3(0, interactableHeight, -4),
-      templateUrl: '/about/intro/intro.html',
+      templateUrl: '/projects/tetris.html',
       options: { color: 'rgb(231, 76, 60)', icon: '/game/map/about.png' },
       name: 'About Me'
     },
     {
       type: OverlayInteractable,
       position: new THREE.Vector3(30, interactableHeight, 17),
-      templateUrl: '/about/testimonials/testimonials.html',
+      templateUrl: '/projects/tetris.html',
       options: { color: 'rgb(243, 156, 18)', icon: '/game/map/album.png' },
       name: 'Album'
     },
     {
       type: OverlayInteractable,
       position: new THREE.Vector3(27, interactableHeight, -12),
-      templateUrl: '/about/fav_games/fav_games.html',
+      templateUrl: '/projects/tetris.html',
       options: { color: 'rgb(241, 196, 15)', icon: '/game/map/fav-games.png' },
       name: 'Favourite Games'
     },
     {
       type: OverlayInteractable,
       position: new THREE.Vector3(40, interactableHeight, -8.7),
-      templateUrl: '/about/likes/likes.html',
+      templateUrl: '/projects/tetris.html',
       options: { color: 'rgb(247, 243, 38)', icon: '/game/map/watch.png' },
       name: 'Watch Me Play'
     },
     {
       type: OverlayInteractable,
       position: new THREE.Vector3(-17, interactableHeight, -24),
-      templateUrl: '/about/likes/likes.html',
+      templateUrl: '/projects/tetris.html',
       options: { color: 'rgb(24, 243, 184)', icon: '/game/map/music.png' },
       name: 'Music'
     },
     {
       type: OverlayInteractable,
       position: new THREE.Vector3(100, interactableHeight, 15),
-      templateUrl: '/about/likes/likes.html',
+      templateUrl: '/projects/tetris.html',
       options: { color: 'rgb(32, 202, 211)', icon: '/game/map/recipes.png' },
       name: 'Recipes'
     },
     {
       type: OverlayInteractable,
       position: new THREE.Vector3(23, interactableHeight, 42),
-      templateUrl: '/about/likes/likes.html',
+      templateUrl: '/projects/tetris.html',
       options: { color: 'rgb(24, 110, 167)', icon: '/game/map/bucket.png' },
       name: 'Bucketlist'
     },
     {
       type: OverlayInteractable,
       position: new THREE.Vector3(0, interactableHeight, -40),
-      templateUrl: '/about/likes/likes.html',
+      templateUrl: '/projects/tetris.html',
       options: { color: 'rgb(150, 28, 198)', icon: '/game/map/happy.png' },
       name: 'What I Like'
     },
     {
       type: OverlayInteractable,
       position: new THREE.Vector3(0, interactableHeight, 40),
-      templateUrl: '/about/likes/likes.html',
+      templateUrl: '/projects/tetris.html',
       options: { color: 'rgb(199, 100, 241)', icon: '/game/map/annoy.png' },
       name: 'What Annoys Me'
     }
@@ -76,28 +76,28 @@ export const GameInteractables = {
     {
       type: OverlayInteractable,
       position: new THREE.Vector3(-24, interactableHeight, 25),
-      templateUrl: '/about/riddles/riddles.html',
+      templateUrl: '/projects/tetris.html',
       options: { color: 'rgb(175, 101, 244)', icon: '/game/map/riddles.gif' },
       name: 'Riddles'
     },
     {
       type: OverlayInteractable,
       position: new THREE.Vector3(-33, interactableHeight, 15),
-      templateUrl: '/about/art/art.html',
+      templateUrl: '/projects/tetris.html',
       options: { color: 'rgb(241, 122, 204)', icon: '/game/map/pixelart.gif' },
       name: 'Pixelart'
     },
     {
       type: OverlayInteractable,
       position: new THREE.Vector3(-45, interactableHeight, 30),
-      templateUrl: '/about/art/art.html',
+      templateUrl: '/projects/tetris.html',
       options: { color: 'rgb(243, 236, 120)', icon: '/game/map/models.gif' },
       name: 'Models'
     },
     {
       type: OverlayInteractable,
       position: new THREE.Vector3(-49, interactableHeight, 35),
-      templateUrl: '/about/art/art.html',
+      templateUrl: '/projects/tetris.html',
       options: { color: 'rgb(248, 185, 126)', icon: '/game/map/textures.gif' },
       name: 'Textures'
     }
@@ -122,7 +122,7 @@ export const GameInteractables = {
     {
       type: OverlayInteractable,
       position: new THREE.Vector3(-110, interactableHeight, 45),
-      templateUrl: '/about/intro/intro.html',
+      templateUrl: '/projects/tetris.html',
       options: { color: 'rgb(129, 255, 196)', icon: '/game/map/say-something.png' },
       name: 'Say Something'
     },
@@ -136,7 +136,7 @@ export const GameInteractables = {
     {
       type: OverlayInteractable,
       position: new THREE.Vector3(0, interactableHeight, 15),
-      templateUrl: '/about/intro/intro.html',
+      templateUrl: '/projects/tetris.html',
       options: { color: 'rgb(255, 255, 255)', icon: '/game/map/contact.png' },
       name: 'Contact'
     }
